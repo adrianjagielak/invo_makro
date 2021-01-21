7c4f4702bbd521ce018eacc24d7031a5492b5cdb
Adrian Jagielak <adrianjagielak@gmail.com>
2021-01-21 13:15:38 +0100

    Add scaffold background color to theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,9 +5,10 @@ const Color primary = Color(0xffF94F36);
 const Color accent = primary;
 
 ThemeData get lightTheme => ThemeData(
-  primaryColor: accent,
-  accentColor: accent,
-  cardTheme: CardTheme(
-    clipBehavior: Clip.antiAlias,
-  ),
-);
+      primaryColor: accent,
+      accentColor: accent,
+      cardTheme: CardTheme(
+        clipBehavior: Clip.antiAlias,
+      ),
+      scaffoldBackgroundColor: Color(0xffF5F5F5),
+    );
